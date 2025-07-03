18a4ce4ec580dcb51fa35b724169654e16feed92
vaibhav-garg <vaibhav.garg@iongroup.com>
2025-07-03 11:10:57 +0000

    podman socKeT update for lazydocker - ld

--- os/podman.nix
+++ os/podman.nix
@@ -10,6 +10,7 @@
       enable = lib.mkForce true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = lib.mkForce true;
+      dockerSocket.enable = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = lib.mkForce true;
       # Periodically prune Podman resources
